b53d8ddc8ab7d0553aee43ff3ed6419523112b59
Georg Höllrigl <georg.hoellrigl@gmx.at>
2023-05-05 21:37:17 +0200

    Backup und Übenahme neue Version wg. EVN Firmware Update

--- EvnSmartmeterMQTT.py
+++ EvnSmartmeterMQTT.py
@@ -1,6 +1,6 @@
+#!/usr/bin/python3
 import serial
 import time
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from binascii import unhexlify
 import sys
 import string
@@ -8,32 +8,53 @@ import paho.mqtt.client as mqtt
 from gurux_dlms.GXDLMSTranslator import GXDLMSTranslator
 from bs4 import BeautifulSoup
 
+from Cryptodome.Cipher import AES
+import xml.etree.ElementTree as ET
+from time import sleep
+
 import subprocess
 
 from evnkeyfile import *
 
 
-#MQTT Verwenden (True | False)
 useMQTT = False
 useMYSQL = False
 useFILE = False
 useTASMOTA = False 
 useTHINGSPEAK = True
 
-#MQTT Broker IP adresse Eingeben ohne Port!
-mqttBroker = "192.168.1.10"
+mqttBroker = "hostname1.localdomain.at"
 mqttuser =""
 mqttpasswort = ""
 mqttport = 1883
 
+#Comport Config
+comport = "/dev/ttyUSB0"
+
 #Aktuelle Werte auf Console ausgeben (True | False)
 printValue = True
 
-#Comport Config/Init
-comport = "/dev/ttyUSB0"
+octet_string_values = {}
+octet_string_values['0100010800FF'] = 'WirkenergieP'
+octet_string_values['0100020800FF'] = 'WirkenergieN'
+octet_string_values['0100010700FF'] = 'MomentanleistungP'
+octet_string_values['0100020700FF'] = 'MomentanleistungN'
+octet_string_values['0100200700FF'] = 'SpannungL1'
+octet_string_values['0100340700FF'] = 'SpannungL2'
+octet_string_values['0100480700FF'] = 'SpannungL3'
+octet_string_values['01001F0700FF'] = 'StromL1'
+octet_string_values['0100330700FF'] = 'StromL2'
+octet_string_values['0100470700FF'] = 'StromL3'
+octet_string_values['01000D0700FF'] = 'Leistungsfaktor'
 
+def evn_decrypt(frame, evn_schluessel, systemTitel, frameCounter):
+    frame = unhexlify(frame)
+    encryption_key = unhexlify(evn_schluessel)
+    init_vector = unhexlify(systemTitel + frameCounter)
+    cipher = AES.new(encryption_key, AES.MODE_GCM, nonce=init_vector)
+    return cipher.decrypt(frame).hex()
 
-#MQTT Init
+# MQTT Init
 if useMQTT:
     try:
         client = mqtt.Client("SmartMeter")
@@ -42,96 +63,112 @@ if useMQTT:
     except:
         print("Die Ip Adresse des Brokers ist falsch!")
         sys.exit()
-    
-
 
 tr = GXDLMSTranslator()
 ser = serial.Serial( port=comport,
          baudrate=2400,
          bytesize=serial.EIGHTBITS,
-         parity=serial.PARITY_NONE,                   
+         parity=serial.PARITY_NONE,
+         stopbits=serial.STOPBITS_ONE
 )
 
 
-
-
-
-
 while 1:
+    print("===================== reading fresh data ======================")
     daten = ser.read(size=282).hex()
-    systemTitel = daten[22:38]
-    frameCounter = daten[44:52]
-    frame = daten[52:560]
 
-    frame = unhexlify(frame)
-    encryption_key = unhexlify(evn_schluessel)
-    init_vector = unhexlify(systemTitel + frameCounter)
-
-
-    aesgcm = AESGCM(encryption_key)
-    apdu = aesgcm.encrypt(init_vector, frame, b'0').hex()
-    
-    try:
-        xml = tr.pduToXml(apdu[:-32],)
-        soup = BeautifulSoup(xml, 'html.parser')
-        results_32 = soup.find_all('uint32')
-        results_16 = soup.find_all('uint16')
-        results_int16 = soup.find_all('int16')
-    except Exception as err:
-        print("Fehler beim Synchronisieren. Programm bitte ein weiteres mal Starten.")
-        print()
-        print("Fehler: ", format(err))
+    print("Daten: ",daten)
+    mbusstart = daten[0:8]
+    print("mbusstart: ",mbusstart," len(hex)=",len(mbusstart)," strlen=",int(len(mbusstart)/2))
+    if mbusstart[0:2] == "68" and mbusstart[2:4] == mbusstart[4:6] and mbusstart[6:8] == "68" :
+        print("ok")
+    else:
+        print("wrong M-Bus Start, restarting")
         sys.exit()
-       
 
-    
+    frameLen=int("0x" + mbusstart[2:4],16)
+    print("frameLen: ",frameLen)
+
+    systemTitel = daten[22:38]
+    print("systemTitel: ",systemTitel," len(hex)=",len(systemTitel)," strlen=",int(len(systemTitel)/2))
+    frameCounter = daten[44:52]
+    print("frameCounter: ",frameCounter," len(hex)=",len(frameCounter)," strlen=",int(len(frameCounter)/2)," = (int) ",int(frameCounter,16))
+    frame = daten[52:12+frameLen*2]
+    print("frame len(hex)=",len(frame)," strlen=",int(len(frame)/2))
+#    print("frame: ",frame)
+
+    apdu = evn_decrypt(frame,evn_schluessel,systemTitel,frameCounter)
+    if apdu[0:6] != "0f8000" :
+        continue
+    else:
+        print("apdu is fine: ",apdu)
+    try:
+        xml = tr.pduToXml(apdu,)
+#        print("xml: ",xml)
+
+        root = ET.fromstring(xml)
+        found_lines = []
+        momentan = []
+
+        items = list(root.iter())
+        for i, child in enumerate(items):
+            if child.tag == 'OctetString' and 'Value' in child.attrib:
+                value = child.attrib['Value']
+                if value in octet_string_values.keys():
+                    if ('Value' in items[i+1].attrib):
+                        if value in ['0100010700FF', '0100020700FF']:
+                            # special handling for momentanleistung
+                            momentan.append(int(items[i+1].attrib['Value'], 16))
+                        found_lines.append({'key': octet_string_values[value], 'value': int(items[i+1].attrib['Value'], 16)});
+
+#        print(found_lines)
+    except BaseException as err:
+        #print("APU: ", format(apdu))
+        print("Fehler: ", format(err))
+        continue;
 
     try:
-        #Wirkenergie A+ in Wattstunden
-        WirkenergiePA = str(results_32[0])
-        WirkenergieP = int(WirkenergiePA[WirkenergiePA.find('=')+2:WirkenergiePA.find('=')+10],16)/1000
+        if len(momentan) == 2:
+            found_lines.append({'key': 'Momentanleistung', 'value': momentan[0]-momentan[1]})
 
-        #Wirkenergie A- in Wattstunden
-        WirkenergieNA = str(results_32[1])
-        WirkenergieN = int(WirkenergieNA[WirkenergieNA.find('=')+2:WirkenergieNA.find('=')+10],16)/1000
+        for element in found_lines:
+            #ConsoleText
+            #if printValue:
+            #    print(element['key']+ ': '+ str(element['value']))
 
-        #Momentanleistung P+ in Watt
-        MomentanleistungPA = str(results_32[2])
-        MomentanleistungP = int(MomentanleistungPA[MomentanleistungPA.find('=')+2:MomentanleistungPA.find('=')+10],16)
+            if element['key'] == "WirkenergieP":
+               WirkenergieP = element['value']/1000
+            if element['key'] == "WirkenergieN":
+               WirkenergieN = element['value']/1000
 
-        #Momentanleistung P- in Watt
-        MomentanleistungNA = str(results_32[3])
-        MomentanleistungN = int(MomentanleistungNA[MomentanleistungNA.find('=')+2:MomentanleistungNA.find('=')+10],16)
+            if element['key'] == "MomentanleistungP":
+               MomentanleistungP = element['value']
+            if element['key'] == "MomentanleistungN":
+               MomentanleistungN = element['value']
 
-        #Spannung L1
-        SpannungL1A = str(results_16[0])
-        SpannungL1 = int(SpannungL1A[SpannungL1A.find('=')+2:SpannungL1A.find('=')+6],16)*0.1
+            if element['key'] == "SpannungL1":
+               SpannungL1 = element['value']*0.1
+            if element['key'] == "SpannungL2":
+               SpannungL2 = element['value']*0.1
+            if element['key'] == "SpannungL3":
+               SpannungL3 = element['value']*0.1
 
-        #Spannung L2
-        SpannungL2A = str(results_16[1])
-        SpannungL2 = int(SpannungL2A[SpannungL2A.find('=')+2:SpannungL2A.find('=')+6],16)*0.1
+            if element['key'] == "StromL1":
+               StromL1 = element['value']*0.01
+            if element['key'] == "StromL2":
+               StromL2 = element['value']*0.01
+            if element['key'] == "StromL3":
+               StromL3 = element['value']*0.01
 
-        #Spannung L3
-        SpannungL3A = str(results_16[2])
-        SpannungL3 = int(SpannungL3A[SpannungL3A.find('=')+2:SpannungL3A.find('=')+6],16)*0.1
+            if element['key'] == "Leistungsfaktor":
+               Leistungsfaktor = element['value']*0.001
 
-        #Strom L1
-        StromL1A = str(results_16[3])
-        StromL1 = int(StromL1A[StromL1A.find('=')+2:StromL1A.find('=')+6],16)*0.01
+    except BaseException as err:
+        print("Fehler: ", format(err))
+        continue;
 
-        #Strom L2
-        StromL2A = str(results_16[4])
-        StromL2 = int(StromL2A[StromL2A.find('=')+2:StromL2A.find('=')+6],16)*0.01
 
-        #Strom L3
-        StromL3A = str(results_16[5])
-        StromL3 = int(StromL3A[StromL3A.find('=')+2:StromL3A.find('=')+6],16)*0.01
-
-        #Leistungsfaktor
-        LeistungsfaktorA = str(results_int16[0])
-        Leistungsfaktor = int(LeistungsfaktorA[LeistungsfaktorA.find('=')+2:LeistungsfaktorA.find('=')+6],16)*0.001
-        
-        
+    try:
         if printValue:
             print('Wirkenergie+: ' + str(WirkenergieP))
             print('Wirkenergie-: ' + str(WirkenergieN))
@@ -145,10 +182,8 @@ while 1:
             print('Strom L3: ' + str(StromL3))
             print('Leistungsfaktor: ' + str(Leistungsfaktor))
             print('Momentanleistung: ' + str(MomentanleistungP-MomentanleistungN))
-            print()
-            print()
-    
-    
+
+
         #MQTT
         if useMQTT:
             client.publish("Smartmeter/WirkenergieP",WirkenergieP)
@@ -163,17 +198,7 @@ while 1:
             client.publish("Smartmeter/StromL2",StromL2)
             client.publish("Smartmeter/StromL3",StromL3)
             client.publish("Smartmeter/Leistungsfaktor",Leistungsfaktor)
-        if useFILE:
-            f = open("/run/smartmeter.html", "w")
-            f.write("<html><head><title>Smartmeter</title> <meta http-equiv=\"refresh\" content=\"3\"> </head>  <body>  </body></html><pre>")
-            f.write("Leistung   : " + str(MomentanleistungP - MomentanleistungN) + " W\n")
-            f.write("Verbraucht : " + str(WirkenergieP) + "\n")
-            f.write("Eingespeist: " + str(WirkenergieN) + "\n")
-            f.write("L1 : " + str(SpannungL1) + "V " + str(StromL1)+ "A\n")
-            f.write("L2 : " + str(SpannungL2) + "V " + str(StromL2)+ "A\n")
-            f.write("L3 : " + str(SpannungL3) + "V " + str(StromL3)+ "A\n")
-            f.close()
-        
+
         if useMYSQL:
             import mysql.connector
 
@@ -221,9 +246,8 @@ while 1:
                 ])
 
     except BaseException as err:
-        print("Fehler beim Synchronisieren. Programm bitte ein weiteres mal Starten.")
+        print("Es ist ein Fehler aufgetreten.")
         print()
         print("Fehler: ", format(err))
 
         sys.exit()
-
